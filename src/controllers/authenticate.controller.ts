25df250c2ebcfed2f92cebc76c7da63a2efdbf1b
Rafael Borges <rafael.borges2698@gmail.com>
2024-07-19 19:41:00 -0300

    config added swc structure and added config to tests e2e

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -6,9 +6,9 @@ import {
 } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { compare } from 'bcryptjs'
-import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
+import { ZodValidationPipe } from '@/pipes/zod-validation.pipe'
   
-  import { PrismaService } from 'src/prisma/prisma.service'
+  import { PrismaService } from '@/prisma/prisma.service'
 import { z } from 'zod'
   
   const authenticateBodySchema = z.object({
