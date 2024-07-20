43b9075687a35faf4802a682abf3b492f6426b2f
Rafael Borges <rafael.borges2698@gmail.com>
2024-07-20 20:58:59 -0300

    tests - added tests e2e to get questions

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,9 +1,7 @@
 import {
-    Body,
-    Controller, Post,
-    Req,
-    UseGuards,
-    UsePipes
+  Body,
+  Controller,
+  Post, UseGuards
 } from '@nestjs/common'
 import { CurrentUser } from '@/auth/current-user-decorator'
 import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
@@ -13,45 +11,44 @@ import { PrismaService } from '@/prisma/prisma.service'
 import { z } from 'zod'
 
 const createQuestionBodySchema = z.object({
-    title: z.string(),
-    content: z.string(),
-  })
-  
-  const bodyValidationType = new ZodValidationPipe(createQuestionBodySchema)
+  title: z.string(),
+  content: z.string(),
+})
 
-  type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>  
+const bodyValidationType = new ZodValidationPipe(createQuestionBodySchema)
 
-  @Controller('/questions')
-  @UseGuards(JwtAuthGuard)
-  export class CreateQuestionController {
-    constructor(private prisma: PrismaService) {}
-    @Post()
-    async handle(@Body(bodyValidationType) body: CreateQuestionBodySchema, @CurrentUser() user: UserPayload ) {
-        
-        
-        const userId = user.sub
-        const { title, content } = body
-        console.log(title, content, 'tests')
-        const slog = this.convertToSlog(title)
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
-        await this.prisma.question.create({
-            data: {
-                authorId: userId,
-                title,
-                content,
-                slog,
-            }
-        })
-    }
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private prisma: PrismaService) {}
+  @Post()
+  async handle(
+    @Body(bodyValidationType) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const userId = user.sub
+    const { title, content } = body
+    console.log(title, content, 'tests')
+    const slog = this.convertToSlog(title)
 
-    private convertToSlog(title: string): string {
-        return title
-          .toLowerCase()
-          .normalize('NFD')
-          .replace(/[\u0300-\u036f]/g, '')
-          .replace(/[^\w\s-]/g, '')
-          .replace(/\s+/g, '-')
-      }
-
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slog,
+      },
+    })
   }
-  
+
+  private convertToSlog(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
+  }
+}
